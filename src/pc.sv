17a4225cf6eea58e7e90c44040f10489e427bb3a
C0 <pierre@fournier.net>
2021-06-21 16:12:01 +0200

    Ajout pc.sv v1

---
+++ src/pc.sv
@@ -0,0 +1,26 @@
+module pc(input  logic        clk,
+          input  logic        reset_n,
+          input  logic        IF,
+          input  logic [1:0]  pc_cmd,
+          input  logic [31:0] pc_v,
+          output logic [31:0] i_address);
+
+      logic [31:0] pc;
+
+      always@(posedge clk)
+        if(negedge reset_n)
+          pc <= '0;
+        else
+          if(IF)
+            begin
+              if(pc_cmd[0] == 1)
+                if(pc_cmd[1] == 1)
+                  pc <= pc_v;
+                else
+                  pc <= pc + pc_v;
+              else
+                pc <= pc + 4;
+            end
+
+      always@*
+        i_address <= pc;
