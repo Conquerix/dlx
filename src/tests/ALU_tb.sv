be62807c35daae93f5c37cbcfd205085c9747df1
C0 <pierre@fournier.net>
2021-06-22 15:39:22 +0200

    Modif noms alu

---
+++ src/tests/ALU_tb.sv
@@ -0,0 +1,20 @@
+module ALU_test();
+    
+    logic[3:0] I;
+    logic EX;
+    logic clk;
+    
+    logic[31:0] E1, E2, S;
+    logic carry,z;
+
+ 
+    ALU ALU1(.I(I),
+             .EX(EX),
+             .clk(clk),
+             .op1(E1),
+             .op2(E2),
+             .res1(E1),
+             .carry(carry),
+             .z(z));
+    
+endmodule
