563009c45190a65c6ee0212186dffe479811103f
Pierre Fournier <pifournier@tp-3a101-04.enst.fr>
2021-07-02 13:18:15 +0200

    asm

--- src_pipeline/driver_vga.sv
+++ src_pipeline/driver_vga.sv
@@ -15,31 +15,48 @@ module driver_vga(input logic clock_50,
 
 
         logic [9:0] sx,sy;
-        logic pixels[59:0][79:0];
+        logic pixels[0:59][0:79];
         logic hsync, vsync, de;
         logic clock_pix = 0;
 
+        wire [6:0] col_w = data[7:1];
+        wire [5:0] lin_w = data[13:8];
+
         always@(posedge clock_50)
-            begin
-                clock_pix <= !clock_pix;
-                if(chip_select & write_enable)
-                    begin
-                        pixels[data[13:8]][data[7:1]] <= data[0];
-                    end
-            end
+        begin
+            if(chip_select & write_enable)
+                begin
+                    pixels[lin_w][col_w] <= data[0];
+                end
+        end
+
+
+        always@(posedge clock_50 or negedge reset_n)
+        if(!reset_n)
+            clock_pix <= 1'b0;
+        else
+            clock_pix <= !clock_pix;
+
+        
+        assign VGA_CLK   = clock_pix;
 
         always@(*)
             begin
-                VGA_CLK   = clock_pix;
                 VGA_HS    = hsync;
                 VGA_VS    = vsync;
                 VGA_BLANK = de;
                 VGA_SYNC  = 0;
             end
 
+        wire [6:0] col_r = sx[9:3];
+        wire [5:0] lin_r = sy[8:3];
+        wire pix_is_one  = pixels[lin_r][col_r];
+
+//`define MIRE
         always_comb
             begin
-                if(pixels[sy[8:3]][sx[9:3]])
+                `ifndef MIRE
+                if(pix_is_one)
                     begin
                         VGA_R = 8'hff;
                         VGA_G = 8'hff;
@@ -51,12 +68,11 @@ module driver_vga(input logic clock_50,
                         VGA_G = 0;
                         VGA_B = 0;
                     end
-                if(!VGA_BLANK)
-                    begin
-                        VGA_R = 0;
-                        VGA_G = 0;
-                        VGA_B = 0;
-                    end
+                `else
+                    VGA_R = {col_r,1'b0};
+                    VGA_G = {lin_r, 2'b00};
+                    VGA_B = '1;
+                `endif
             end
         
         simple_display_timings_480p display_timings(.clock_pix(clock_pix),
